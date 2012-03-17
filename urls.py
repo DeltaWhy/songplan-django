a6d17fe64feedd3c32169cb88f74fadf92d49b66
Michael Limiero <mike5713@gmail.com>
2012-03-16 20:01:47 -0400

    added simple homepage

--- urls.py
+++ urls.py
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^$', 'django.views.generic.simple.redirect_to', {'url': '/songs/'}),
+    #url(r'^$', 'django.views.generic.simple.redirect_to', {'url': '/songs/'}),
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^songs/', include('songplan.songs.urls')),
     url(r'^sets/', include('songplan.songsets.urls')),
