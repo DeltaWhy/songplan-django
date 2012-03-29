ef8087a919dc61cc4aef5928a7bd328024b90801
Michael Limiero <mike5713@gmail.com>
2012-03-29 15:55:11 -0400

    added set music view

--- songsets/urls.py
+++ songsets/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^$', 'django.views.generic.list_detail.object_list', dict(info_dict, template_name="sets_list.html")),
     url(r'^(?P<object_id>[\d]+)/$', 'django.views.generic.list_detail.object_detail',
         dict(info_dict, template_name="set_detail.html"), name='set_detail'),
+    url(r'^(?P<object_id>[\d]+)/music/$', 'songsets.views.music', name='set_music_view'),
     #url(r'^(?P<slug>[-\w]+)/(?P<key>[A-G][+-]?)/$', 'songs.views.detail'),
 )
--- songsets/views.py
+++ songsets/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from songs.models import Song
+from songsets.models import *
+from songplan.songs import helpers
+def music(request, object_id):
+    songset = get_object_or_404(SongSet, pk=object_id)
+    songs = []
+    for item in songset.setitem_set.all():
+        chords = helpers.transpose(item.song.original_key, item.key, item.song.chords)
+        headerlines = helpers.headerlines(chords)
+        songs.append({'song': item.song, 'key': item.key, 'chords': chords, 
+                    'headerlines': headerlines})
+    return render_to_response('set_music.html', {'songs': songs})
